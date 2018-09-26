b263f0c8c3946cfeff85d6e042b6655528e05e74
MasterFred <fredericstojicevic@gmail.com>
2018-09-26 19:48:24 +0200

    Ajout des exos Ruby 10 et 11.

---
+++ exo_10.rb
@@ -0,0 +1,4 @@
+puts "Quel est ton année de naissance ?"
+print "> "
+birthYear = Integer(gets.chomp)
+puts "Ton âge en 2017 était : #{2017 - birthYear}" + " ans."
---
+++ exo_11.rb
@@ -0,0 +1,7 @@
+puts "Donne moi un nombre :"
+print "> "
+number = Integer(gets.chomp)
+
+number.times do
+	puts "Salut, ça farte ?"
+end
