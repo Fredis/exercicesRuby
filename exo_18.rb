6562ec34f06fffd5c62418a5c51726f5e032e86a
MasterFred <fredericstojicevic@gmail.com>
2018-09-27 11:36:00 +0200

    Ajout de l'exercice 18 en Ruby.

---
+++ exo_18.rb
@@ -0,0 +1,18 @@
+emails = []
+value = 1
+
+50.times do
+	if value < 10
+		conversion = sprintf("%02d", value)
+		finalFormat = "jean.dupont."+conversion+"@email.fr"
+
+	else
+		finalFormat = "jean.dupont.#{value}@email.fr"
+	end
+	emails << finalFormat
+	value = value + 1
+end
+
+emails.each do |emailValue|
+	puts emailValue
+end
